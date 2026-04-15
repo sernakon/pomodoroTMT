aed042479cc38ee2b4ed2d90e66709210926b3a2
Serhii Nakonechnyi <sernakgc@gmail.com>
2026-04-15 16:45:45 +0300

    add timer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from typing import Container
 
 from PyQt6.QtWidgets import QApplication, QLabel, QLineEdit, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtCore import QSize, QTimer, Qt
 
 import sys
 
@@ -12,29 +11,57 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
 
-        self.button = QPushButton("Erase~~")
-        self.button.setCheckable(True)
-        self.button.clicked.connect(self.btn_clicked)
+        self.button_start = QPushButton("START")
+        self.button_start.setCheckable(True)
+        self.button_start.clicked.connect(self.btn_clicked)
 
-        self.label = QLabel()
-        self.input = QLineEdit()
-        self.input.textChanged.connect(self.label.setText)
+        self.button_settings = QPushButton("settings")
+        self.button_settings.setFixedWidth(120)
+
+        self.main_timer = QLabel("24:59")
+        self.main_timer.setFixedHeight(70)
+        self.main_timer.setStyleSheet("font-size:64px;")
+        self.main_timer.setAlignment(Qt.AlignmentFlag.AlignCenter)
         
+        self.off_timer = QLabel("15:00")
+        self.off_timer.setStyleSheet("font-size:24px;")
+        self.off_timer.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+
         self.setFixedSize(QSize(300, 400))
 
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.input)
-        layout.addWidget(self.label)
-        layout.addWidget(self.button)
+        layout = QVBoxLayout() 
+        layout.addWidget(self.button_settings)
+        layout.addStretch()
+        layout.addWidget(self.main_timer)
+        layout.addWidget(self.off_timer)
+        layout.addStretch()
+        layout.addWidget(self.button_start)
 
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
 
+        self.seconds = 66
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer_upd)
+
+    def timer_upd(self):
+        self.seconds -= 1
+        m = self.seconds//60
+        s = self.seconds%60
+        self.main_timer.setText(f'{m:02d}:{s:02d}')
+        if self.seconds <= 0:
+            self.main_timer.setText("next cycle")
 
     def btn_clicked(self):
-        self.input.setText("")
+        if self.button_start.text() == "START":
+            self.button_start.setText("STOP")
+            self.timer.start(1000)
+        else:
+            self.button_start.setText("START")
+            self.timer.stop()
 
 
 
